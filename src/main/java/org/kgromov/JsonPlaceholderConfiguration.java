0558dd2475a42dfadee8458a2e3844c15be9a0f6
konstantin <konst.gromov@gmail.com>
2025-11-29 02:37:02 +0200

    Change not working value to confiuration properties

--- src/main/java/org/kgromov/JsonPlaceholderConfiguration.java
+++ src/main/java/org/kgromov/JsonPlaceholderConfiguration.java
@@ -3,20 +3,19 @@ package org.kgromov;
 import org.kgromov.client.CommentClient;
 import org.kgromov.client.PostClient;
 import org.kgromov.client.UserClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestClient;
 
+@EnableConfigurationProperties(JsonPlaceholderServiceProperties.class)
 @AutoConfiguration
 public class JsonPlaceholderConfiguration {
 
     @Bean("jsonPlaceholderRestClient")
-    RestClient restClient(@Value("spring.json.placeholder.base-url")
-                          @DefaultValue("https://jsonplaceholder.typicode.com") String baseUrl) {
+    RestClient restClient(JsonPlaceholderServiceProperties properties) {
         return RestClient.builder()
-                .baseUrl(baseUrl)
+                .baseUrl(properties.baseUrl())
                 .build();
     }
 
---
+++ src/main/java/org/kgromov/JsonPlaceholderServiceProperties.java
@@ -0,0 +1,13 @@
+package org.kgromov;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+@ConfigurationProperties("json-placeholder-service")
+public record JsonPlaceholderServiceProperties(
+        @DefaultValue("https://jsonplaceholder.typicode.com")
+        String baseUrl
+) {
+
+}
